e94d277f4906f8a2f74a019669a12e5eb28357d7
tsavou <theosavourat@gmail.com>
2025-04-14 00:17:09 +0200

    feat(freelance): freelance controller and routes
    - Added FreelanceController with index and show methods
    - Updated routes to include freelance index and show routes
    - Updated middleware to load freelance data for authenticated users
    - Updated VerificationBanner component to display freelance verification status
    - Updated Freelance/Index and Freelance/Show Vue components to display freelance data"

---
+++ resources/js/Pages/Freelance/Show.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Layout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head } from '@inertiajs/vue3';
+
+defineProps({
+    freelance: Object,
+});
+</script>
+
+<template>
+    <Layout>
+        <Head title="Show" />
+
+        {{freelance}}
+    </Layout>
+</template>
