b311fd9f8b9ad7d57615c559735b90fbbe5d3647
tsavou <theosavourat@gmail.com>
2025-04-27 23:53:02 +0200

    feat: update PrimaryButton component
    - Update PrimaryButton component to support different element types (button, a, Link)
    - Add Link component to app.js for global usage

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,7 +1,7 @@
 import '../css/app.css';
 import './bootstrap';
 
-import { createInertiaApp } from '@inertiajs/vue3';
+import { createInertiaApp, Link } from '@inertiajs/vue3';
 import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
 import { createApp, h } from 'vue';
 import { ZiggyVue } from '../../vendor/tightenco/ziggy';
@@ -19,6 +19,7 @@ createInertiaApp({
         return createApp({ render: () => h(App, props) })
             .use(plugin)
             .use(ZiggyVue)
+            .component('Link', Link)
             .mount(el);
     },
     progress: {
