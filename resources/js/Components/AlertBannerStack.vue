5f029ce692cb804239026a496e5b2a3961e26580
tsavou <theosavourat@gmail.com>
2025-05-12 16:49:46 +0200

    fix(AlertBannerStack): add z-index to improve alert superposition

--- resources/js/Components/AlertBannerStack.vue
+++ resources/js/Components/AlertBannerStack.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="pointer-events-none fixed inset-x-0 bottom-0 flex flex-col gap-3 sm:px-6 sm:pb-5 lg:px-8"
+        class="pointer-events-none fixed inset-x-0 bottom-0 z-50 flex flex-col gap-3 sm:px-6 sm:pb-5 lg:px-8"
     >
         <slot />
     </div>
