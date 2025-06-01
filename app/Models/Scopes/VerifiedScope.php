7b33430c431b9f5e7f50c1dc0bc0c603240ea2af
tsavou <theosavourat@gmail.com>
2025-06-01 04:44:01 +0200

    fix(VerifiedScope): Ensure users can edit their own freelance profile even if not verified
    - Update VerifiedScope to include the authenticated user's freelance regardless of verification status
    - Ensure users can edit their own profile even if not verified

--- app/Models/Scopes/VerifiedScope.php
+++ app/Models/Scopes/VerifiedScope.php
@@ -5,11 +5,23 @@ namespace App\Models\Scopes;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Scope;
+use Illuminate\Support\Facades\Auth;
 
 class VerifiedScope implements Scope
 {
     public function apply(Builder $builder, Model $model)
     {
+        if (Auth::check()) {
+            // Si l'utilisateur est connecté, on sélectionne les freelances vérifiés ou le freelance connecté
+            $userId = Auth::id();
+
+            $builder->where(function ($query) use ($userId) {
+                $query->where('is_verified', true)
+                    ->orWhere('user_id', $userId);
+            });
+            return;
+        }
+
         $builder->where('is_verified', true);
     }
 }
